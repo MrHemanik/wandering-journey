479d7178b13223ceba26d1f00469c2ebe5bfe0ec
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-08-30 11:22:41 +0200

    Put colors in record so Color doesn't expose (..) and change usages accordingly

--- reload.ps1
+++ reload.ps1
@@ -1,1 +1,4 @@
 ﻿Invoke-Expression "elm make src/Main.elm --output wanderingJourney.js"
+
+# For Develop: "elm make src/Main.elm --output wanderingJourney.js"
+# For Release: "elm make src/Main.elm --optimize --output wanderingJourney.js"
